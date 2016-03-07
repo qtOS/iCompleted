9dc46a5517e1a2b201bbff31d4b7637ffa5794af
Aaron <akr.git.receiver@gmail.com>
2016-03-07 14:08:34 -0600

    angular is up and running -- full CRUD

--- public/javascripts/angular-app.js
+++ public/javascripts/angular-app.js
@@ -1,18 +1,23 @@
 var iCompleted = angular.module('iCompleted', []);
-
+iCompleted.config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('//');
+    $interpolateProvider.endSymbol('//');
+});
 
 iCompleted.controller('GoalListCtrl', function ($scope, $http) {
 
   $scope.fetch = function() {
     $http.get('/goals').success(function(data) {
-      console.log('fetched data')
-      $scope.tasks = data;
+      console.log('fetched the data');
+      $scope.goals = data;
+      console.log(data[1]);
     });
   };
 
   $scope.createGoal = function(name, description) {
     $http.post("/goals", {name : name, description: description}).success(function(data, status) {
         $scope.fetch();
+        console.log('created goal');
     });
   };
 
@@ -21,7 +26,7 @@ iCompleted.controller('GoalListCtrl', function ($scope, $http) {
     var answer = confirm("Are you sure you want to complete this task?");
     if (answer == true) {
       console.log("Deleted Task with _id:" + goal["_id"]);
-      $http.delete('/' + goal["_id"]);
+      $http.delete('/goals/' + goal["_id"]);
       $scope.fetch();
     }
 
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-  Goal.find(function (err, goals) {
-    if (err) return next(err);
-    res.json(goals);
-    console.log('found a goal');
-  });
 });
 
 module.exports = router;
