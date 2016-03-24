8a695899126902b7671c4e7c213e3552baf60e1f
Aaron <akr.git.receiver@gmail.com>
2016-03-24 09:52:02 -0500

    updated a lot, added more pages, more UX func, more love

--- public/javascripts/temp.js
+++ public/javascripts/temp.js
@@ -1,7 +1,91 @@
 $(function(){
-  $("#nav-btn").click(function(){
-    $('#login-container').css({
-      'display': 'block'
-    })
-  });
+  var $navbtn = $('#nav-btn'),
+      $userbtn = $('#user-btn'),
+      $exitlogin = $('#exit-login'),
+      $navcont = $('#navigation-container'),
+      $loadingPanel = $('#loading-panel'),
+      $logincont = $('#login-container');
+
+  var NavDisOn = {
+    "opacity": 1,
+    "right": "10%",
+  },
+  NavDisOff = {
+    "opacity": 0,
+    "right": "-40%"
+  }
+  var ctr = 0;
+  var processing = false;
+
+  //~~~~~~~~~~ Nav animation
+  $navbtn.click(function(){
+    if(0 === ctr % 2 && processing == false){
+      ctr++;
+      processing = true;
+      //$loadingPanel.show();
+      console.log(ctr + ": even");
+      $navbtn.removeClass('not');
+      $navbtn.addClass('active');
+      $navcont.animate(NavDisOn, 1250, function(){
+        console.log("open" + ": "+processing);
+        processing = false;
+        //$loadingPanel.hide();
+      });
+    }else if(1 ==ctr % 2 && processing == false){
+      ctr++;
+      processing = true;
+      //$loadingPanel.show();
+      $navbtn.removeClass('active');
+      $navbtn.addClass('not');
+        $navcont.animate(NavDisOff,550, function(){
+          console.log('closed'+ ": "+processing);
+          processing = false;
+          //$loadingPanel.hide();
+        });
+      console.log(ctr + ": odd")
+    }else{
+      //$loadingPanel.show()
+      processing == true;
+      if(processing == false){
+        alert('error!');
+      }
+    }
+  })
+  //~~~~~~~~ end of Nav animation
+  //~~~~~~~~ Beginning userlogin
+  var open = false;
+  $userbtn.click(function(){
+    if(open == false){
+      console.log('we got clicks: ')
+      $logincont.css({"display": "block"});
+      open = true;
+      if(open == true){
+        $exitlogin.click(function(){
+          console.log('clicked')
+          $logincont.css({"display": "none"});
+          open = false;
+        });
+      }
+    }
+  })
+
+
+
+  // console.log($(".refLi"));
+  // function goToByScroll(id){
+  //          // Reove "link" from the ID
+  //        id = id.replace("link", "");
+  //          // Scroll
+  //        $('#goals-ul > li').animate({
+  //            scrollTop: $("#"+id).offset().top},
+  //            'slow');
+  //    };
+  // $("li > a").click(function() {
+  //       // Prevent a page reload when a link is pressed
+  //     e.preventDefault();
+  //       // Call the scroll function
+  //     console.log('clicked');
+  //     goToByScroll($(this).attr("href"));
+  // });
+  // console.log('passed the shit');
 });
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,26 @@ var https = require('https');
 //var Account = require("../models/Account")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { user: req.user, name: req.email});
+router.get('/', function(req, res, next){
+  res.render('landing-page', {
+    title: 'AKQT',
+    anotherThing: 'WHOOP WHOOP'
+  });
 });
+//blog
+router.get('/blog', function(req, res, next) {
+  res.render('index', { user: req.user, name: req.email, title: 'Subject+Thought'});
+});
+router.get('/about', function(req, res, next){
+  res.render('about', {
+    author: "Aaron Krueger",
+    age: "21",
+    linkedInProfile: "https://www.linkedin.com/in/cssshaman",
+    //change this url later
+    imgUrl: "public/images/name.png",
+    githubUrl: "https://github.com/y04nqt",
+    title: 'About me'
+  });
+})
 
 module.exports = router;
