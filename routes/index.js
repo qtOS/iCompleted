0b75248d8dfb305b030ddce97094f14b4dc241fc
Aaron <akr.git.receiver@gmail.com>
2016-04-28 21:55:51 -0400

    updated some style and added more to portfolio

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,14 @@ var basicInfo = {
       githubUrl: "https://github.com/y04nqt",
       linkedInProfile: "https://www.linkedin.com/in/cssshaman",
       imgUrl: "./images/profile.png",
-      devStatement: "Digital technology is defining our lives by recording our actions daily. Whether we know it or not, our lives are being analysed by hundreds of algorithms that directly affect us. It is my mission to use data effectively and efficiently to deliver a solution to the user for their benefit. I believe it all begins with the user needing a solution to a problem they don't realize they have. These problems lead to outstanding solutions for others and creates effective outcomes."
+      devStatement: "I am an ambitious zealot of JS and mastermind of CSS. I strive to learn new frameworks consistently and put forward elegantly fast applications. Most of all my styling work is contrived personally without bootstrap or clunky plugins. It is crucial to have beautifully efficient interfaces and interactions for users to enjoy. I make it my goal, no matter the project, to create intuitive layouts and architecture to ease use for all users. First and foremost, focusing on data-driven applications ready for mobile devices using web-based technology. You may download my résumé from the portfolio page by clicking the flashing title."
     },
     portfolioLinks = {
+      shoppingMallJapan:{
+        name: "Shopping Mall Japan",
+        description: "A web-based technology facelift for a dated UI deputy service business site. Currently under reconstruction.",
+        link: "http://www.shoppingmalljapan.com/"
+      },
       runningM8: {
         name: "RunningM8",
         description: "A Ruby/Sinatra application with a homemade user interface and design. This app is meant for a user who is training for a marathon to track their miles ran and keep them on track with their goal. It has two levels, novice and intermediate. The user then inputs the miles ran each day and the app will update the calendar as time passes. It includes data validation, custom animation, and full user interface.",
